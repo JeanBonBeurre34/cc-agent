6383f14eda8fbd20ee33ae44f6a4a07778e028cf
Reg0l <129545703+JeanBonBeurre34@users.noreply.github.com>
2024-04-02 15:48:50 +0200

    Update main.go
    As user i can pull the command every minutes

--- main.go
+++ main.go
@@ -1,159 +1,101 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"sync"
-	"time"
-	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/debug"
-	"golang.org/x/sys/windows/svc/eventlog"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "sync"
+    "time"
 )
 
-var elog debug.Log
-
 type Command struct {
-	ID  string `json:"id"`
-	Cmd string `json:"cmd"`
+    ID  string `json:"id"`
+    Cmd string `json:"cmd"`
 }
 
 type CommandResult struct {
-	ID     string `json:"id"`
-	Result string `json:"result"`
+    ID     string `json:"id"`
+    Result string `json:"result"`
 }
 
-type myService struct{}
-
 var (
-	mu               sync.Mutex
-	isCommandRunning bool
+    mu               sync.Mutex
+    isCommandRunning bool
 )
 
 func fetchCommand() (Command, error) {
-	var cmd Command
-	resp, err := http.Get("http://localhost:5000/command")
-	if err != nil {
-		return cmd, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return cmd, err
-	}
-	err = json.Unmarshal(body, &cmd)
-	return cmd, err
+    var cmd Command
+    resp, err := http.Get("http://192.168.56.101:5000/command")
+    if err != nil {
+        return cmd, err
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return cmd, err
+    }
+    err = json.Unmarshal(body, &cmd)
+    return cmd, err
 }
 
 func sendResult(result CommandResult) error {
-	jsonData, err := json.Marshal(result)
-	if err != nil {
-		return err
-	}
+    jsonData, err := json.Marshal(result)
+    if err != nil {
+        return err
+    }
 
-	resp, err := http.Post("http://localhost:5000/submit_result", "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return nil
+    resp, err := http.Post("http://192.168.56.101:5000/submit_result", "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+    return nil
 }
 
 func executeCommandAndSendResult(cmd Command) {
-	resultText := fmt.Sprintf("Executed command: %s", cmd.Cmd)
+    resultText := fmt.Sprintf("Executed command: %s", cmd.Cmd)
 
-	result := CommandResult{
-		ID:     cmd.ID,
-		Result: resultText,
-	}
+    result := CommandResult{
+        ID:     cmd.ID,
+        Result: resultText,
+    }
 
-	err := sendResult(result)
-	if err != nil {
-		elog.Error(1, fmt.Sprintf("Error sending result for command ID %s: %s", cmd.ID, err))
-	} else {
-		elog.Info(1, fmt.Sprintf("Result for command ID %s sent successfully", cmd.ID))
-	}
-}
-
-func (m *myService) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (svcSpecificEC bool, exitCode uint32) {
-	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
-	changes <- svc.Status{State: svc.StartPending}
-	ticker := time.NewTicker(5 * time.Minute)
-	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
-
-	go func() {
-		for range ticker.C {
-			mu.Lock()
-			if !isCommandRunning {
-				isCommandRunning = true
-				mu.Unlock()
-
-				cmd, err := fetchCommand()
-				if err != nil {
-					elog.Error(1, fmt.Sprintf("Error fetching command: %s", err))
-				} else if cmd.ID != "" {
-					executeCommandAndSendResult(cmd)
-				}
-
-				mu.Lock()
-				isCommandRunning = false
-			}
-			mu.Unlock()
-		}
-	}()
-
-loop:
-	for {
-		select {
-		case c := <-r:
-			switch c.Cmd {
-			case svc.Interrogate:
-				changes <- c.CurrentStatus
-			case svc.Stop, svc.Shutdown:
-				changes <- svc.Status{State: svc.StopPending}
-				ticker.Stop()
-				break loop
-			default:
-				elog.Error(1, fmt.Sprintf("Unexpected control request #%d", c.Cmd))
-			}
-		}
-	}
-
-	return false, 0
-}
-
-func runService(name string, isDebug bool) {
-	var err error
-	if isDebug {
-		elog = debug.New(name)
-	} else {
-		elog, err = eventlog.Open(name)
-		if err != nil {
-			os.Exit(1)
-		}
-	}
-	defer elog.Close()
-
-	err = svc.Run(name, &myService{})
-	if err != nil {
-		elog.Error(1, fmt.Sprintf("Service failed: %v", err))
-	}
+    err := sendResult(result)
+    if err != nil {
+        fmt.Printf("Error sending result for command ID %s: %s\n", cmd.ID, err)
+    } else {
+        fmt.Printf("Result for command ID %s sent successfully\n", cmd.ID)
+    }
 }
 
 func main() {
-	isInteractive, err := svc.IsAnInteractiveSession()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to determine if we are running in an interactive session: %v", err)
-		return
-	}
-	if !isInteractive {
-		serviceName := "MyGoService"
-		runService(serviceName, false)
-		return
-	}
-	fmt.Fprintf(os.Stderr, "Running in debug mode\n")
-	runService("MyGoServiceDebug", true)
+    fmt.Println("Application started. Press CTRL+C to exit.")
+    ticker := time.NewTicker(1 * time.Minute)
+    go func() {
+        for range ticker.C {
+            mu.Lock()
+            if !isCommandRunning {
+                isCommandRunning = true
+                mu.Unlock()
+
+                cmd, err := fetchCommand()
+                if err != nil {
+                    fmt.Printf("Error fetching command: %s\n", err)
+                    continue
+                }
+                if cmd.ID != "" {
+                    executeCommandAndSendResult(cmd)
+                }
+
+                mu.Lock()
+                isCommandRunning = false
+            }
+            mu.Unlock()
+        }
+    }()
+
+    // Prevent the application from exiting immediately
+    select {}
 }
